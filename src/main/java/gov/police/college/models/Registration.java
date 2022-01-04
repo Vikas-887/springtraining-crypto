507e30d1fc427b0a19dd9ccaba479a71a6c1a7d4
Vikas-887 <vks998877@gmail.com>
2022-01-04 12:18:24 +0530

    registration file constructor  getter and setter

--- src/main/java/gov/police/college/models/Registration.java
+++ src/main/java/gov/police/college/models/Registration.java
@@ -10,9 +10,66 @@ public class Registration {
 
     private String name;
     private LocalDateTime dob;
+    
     private String course;
     private String academicSession;
     private String address;
     private String mobileNumber;
     private String fatherName;
+    public Registration(String name, LocalDateTime dob, String course, String academicSession, String address,
+            String mobileNumber, String fatherName) {
+        this.name = name;
+        this.dob = dob;
+        this.course = course;
+        this.academicSession = academicSession;
+        this.address = address;
+        this.mobileNumber = mobileNumber;
+        this.fatherName = fatherName;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getFatherName() {
+        return fatherName;
+    }
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getAcademicSession() {
+        return academicSession;
+    }
+    public void setAcademicSession(String academicSession) {
+        this.academicSession = academicSession;
+    }
+    public String getCourse() {
+        return course;
+    }
+    public void setCourse(String course) {
+        this.course = course;
+    }
+    public LocalDateTime getDob() {
+        return dob;
+    }
+    public void setDob(LocalDateTime dob) {
+        this.dob = dob;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+ 
+
+    
 }
